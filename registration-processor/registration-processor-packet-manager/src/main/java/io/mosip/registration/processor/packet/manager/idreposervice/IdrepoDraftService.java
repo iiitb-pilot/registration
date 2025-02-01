77128b7802f9100444a4bd024d2e48da38161a5a
Nambi <nambirajan@mosip.io>
2025-02-02 00:07:28 +0530

    Temp code changes

--- registration-processor/registration-processor-packet-manager/src/main/java/io/mosip/registration/processor/packet/manager/idreposervice/IdrepoDraftService.java
+++ registration-processor/registration-processor-packet-manager/src/main/java/io/mosip/registration/processor/packet/manager/idreposervice/IdrepoDraftService.java
@@ -103,8 +103,13 @@ public class IdrepoDraftService {
         IdResponseDTO response = (IdResponseDTO) registrationProcessorRestClientService.patchApi(
                 ApiName.IDREPOUPDATEDRAFT, Lists.newArrayList(id), null, null, idRequestDto, IdResponseDTO.class);
         if (response.getErrors() != null && !response.getErrors().isEmpty()) {
-            String requestString = mapper.writeValueAsString(idRequestDto);
-            regProcLogger.info("Request sent for patch API - " + id + " - " + requestString);
+            JSONObject newIdentity = mapper.readValue(mapper.writeValueAsString(idRequestDto.getRequest().getIdentity()), JSONObject.class);
+            String reqUin = mapper.writeValueAsString(newIdentity.get(UIN));
+            String verifiedAttribute = mapper.writeValueAsString(newIdentity.get("verifiedAttributes"));
+            String registrationId = mapper.writeValueAsString(newIdentity.get("registrationId"));
+            regProcLogger.info("Request sent for patch API - UIN " + id + " - " + reqUin);
+            regProcLogger.info("Request sent for patch API - VerifiedAttributes " + id + " - " + verifiedAttribute);
+            regProcLogger.info("Request sent for patch API - RegistrationId " + id + " - " + registrationId);
 			ErrorDTO error = response.getErrors().get(0);
 			regProcLogger.error("Error occured while updating draft for id : " + id, error.toString());
 			if (response.getErrors().get(0).getErrorCode().equalsIgnoreCase(ID_REPO_KEY_MANAGER_ERROR)) {
