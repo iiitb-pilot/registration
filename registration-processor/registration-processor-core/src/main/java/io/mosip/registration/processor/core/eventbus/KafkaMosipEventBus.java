e9794dd4f71eabdf52bf9b8511d3a80155228d87
trialblazerseee <84778104+trialblazerseee@users.noreply.github.com>
2025-05-21 15:56:02 +0530

    Adding Caffine cache for caching RID

--- registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/eventbus/KafkaMosipEventBus.java
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/eventbus/KafkaMosipEventBus.java
@@ -6,9 +6,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.apache.camel.com.github.benmanes.caffeine.cache.Cache;
+import org.apache.camel.com.github.benmanes.caffeine.cache.Caffeine;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.slf4j.MDC;
@@ -65,6 +68,8 @@ public class KafkaMosipEventBus implements MosipEventBus {
 
 	private EventTracingHandler eventTracingHandler;
 
+	private final Cache<String, String> cache;
+
 	/**
 	 * Instantiates a new kafka mosip event bus.
 	 *
@@ -83,6 +88,10 @@ public class KafkaMosipEventBus implements MosipEventBus {
 			String commitType, String maxPollRecords, String maxPollinterval, int pollFrequency, EventTracingHandler eventTracingHandler) {
 
 		validateCommitType(commitType);
+		this.cache = Caffeine.newBuilder()
+				.expireAfterWrite(15, TimeUnit.MINUTES)
+				.maximumSize(100000) // optional: set a size limit
+				.build();
 		this.vertx = vertx;
 		this.commitType = commitType;
 		this.pollFrequency = pollFrequency;
@@ -261,6 +270,14 @@ public class KafkaMosipEventBus implements MosipEventBus {
 			.compose((Void) -> {
 				List<Future<Void>> futures = IntStream.range(0, consumerRecords.size())
 					.mapToObj(consumerRecords::recordAt)
+						.filter(record -> {
+							String key = record.key();
+							if (key != null && isCacheExist(key)) {
+								logger.error("Duplicate record with key '{}' found. Skipping processing.", key);
+								return false;
+							}
+							return true;
+						})
 					.map(record -> processRecord(toAddress, eventHandler, record, false))
 					.collect(Collectors.toList());
 
@@ -469,5 +486,45 @@ public class KafkaMosipEventBus implements MosipEventBus {
 			healthCheckDTO.setFailureReason("Failed kafkaProducer");
 		}
 		eventHandler.handle(healthCheckDTO);
+	}
+
+	/**
+	 * Adds a string to the cache with a specific key.
+	 *
+	 * @param key   The key for the cache entry.
+	 * @param value The string value to be cached.
+	 */
+	public void addToCache(String key, String value) {
+		cache.put(key, value);
+	}
+
+	/**
+	 * Gets a value from the cache by key.
+	 *
+	 * @param key The key to retrieve.
+	 * @return The cached string or null if not present or expired.
+	 */
+	public String getFromCache(String key) {
+		return cache.getIfPresent(key);
+	}
+
+	/**
+	 * Removes a key from the cache immediately.
+	 *
+	 * @param key The key to remove.
+	 */
+	public void removeFromCache(String key) {
+		cache.invalidate(key);
+	}
+
+	public boolean isCacheExist(String rid) {
+		String val = getFromCache(rid);
+		if(val != null) {
+			logger.info("Caffine Cache Value is " + val);
+			return true;
+		} else {
+			addToCache(rid, "1");
+			return false;
+		}
 	}
 }
