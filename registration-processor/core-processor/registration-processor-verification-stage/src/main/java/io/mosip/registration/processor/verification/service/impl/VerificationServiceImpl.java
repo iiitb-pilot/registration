2b70013687348420fa247c0d9f597c0b4bc624fb
User <smrutiranjan.samal@infystrat.com>
2026-01-16 13:08:33 +0530

    stop sucess flow

--- registration-processor/core-processor/registration-processor-verification-stage/src/main/java/io/mosip/registration/processor/verification/service/impl/VerificationServiceImpl.java
+++ registration-processor/core-processor/registration-processor-verification-stage/src/main/java/io/mosip/registration/processor/verification/service/impl/VerificationServiceImpl.java
@@ -286,7 +286,7 @@ public class VerificationServiceImpl implements VerificationService {
 			regProcLogger.error(ExceptionUtils.getStackTrace(e));
 			regProcLogger.debug(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.REGISTRATIONID.toString(),
 					e.getMessage(), e.getMessage());
-		} finally {
+		} /*finally {
 			if (isTransactionSuccessful) {
 				messageDTO.setIsValid(true);
 				description.setCode(PlatformSuccessMessages.RPR_VERIFICATION_SUCCESS.getCode());
@@ -295,7 +295,7 @@ public class VerificationServiceImpl implements VerificationService {
 				registrationStatusDto.setSubStatusCode(StatusUtil.VERIFICATION_FAILED.getCode());
 			updateStatus(messageDTO, registrationStatusDto, isTransactionSuccessful, description,
 					PlatformSuccessMessages.RPR_VERIFICATION_SENT);
-		}
+		}*/
 
 		regProcLogger.debug(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.REGISTRATIONID.toString(),
 				messageDTO.getRid(), "VerificationServiceImpl::process()::entry");
